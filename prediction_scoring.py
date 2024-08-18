627ec79c668d8926f7bc5744d777089cb0c3725c
ZiedTrikiDataScience <triki.zied.aiesec@gmail.com>
2024-08-18 18:01:57 +0100

    Added Extra pre-commit cheatsheetand files

--- prediction_scoring.py
+++ prediction_scoring.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import requests
 
-url = 'http://localhost:5001/predict'
+url = "http://localhost:5001/predict"
 
 # Path to your Excel file
 excel_file_path = r"data_to_test_and_predict.xlsx"
@@ -12,31 +12,27 @@ df = pd.read_excel(excel_file_path)
 print(f"Loaded dataset with {len(df)} rows")
 
 # Prepare the file for sending
-files = {'file': open(excel_file_path, 'rb')}
-
+files = {"file": open(excel_file_path, "rb")}
 
 
 # Send the request
 response = requests.post(url, files=files)
 
 
-
-
-
 if response.status_code == 200:
     result = response.json()
-    predictions = result['predictions']
-    results = result['results']
-    
+    predictions = result["predictions"]
+    results = result["results"]
+
     # Add predictions and results to the dataframe
-    df['Prediction'] = predictions
-    df['Result'] = results
-    
+    df["Prediction"] = predictions
+    df["Result"] = results
+
     # Display results
     print(df)
-    
+
     # Optionally, save the results to a new Excel file
-    df.to_excel('prediction_results.xlsx', index=False)
+    df.to_excel("prediction_results.xlsx", index=False)
     print("Results saved to 'prediction_results.xlsx'")
 else:
     print(f"Error: {response.json()['error']}")
